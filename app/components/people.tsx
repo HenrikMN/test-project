ec416ff3a596866bef8f42986002b8062b6954fb
Adrian-kodehode <adrian.kodehode@gmail.com>
2025-03-25 14:00:03 +0100

    .

---
+++ app/components/people.tsx
@@ -0,0 +1,30 @@
+export const people = [
+  {
+    name: "Leslie Alexander",
+    role: "Co-Founder / CEO",
+    email: "",
+    imageUrl:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+      lastSeen: "3h ago",
+      lastSeenDateTime: "2023-01-23T13:23Z",
+  },
+  {
+    name: "Hatsune Miku",
+    email: "",
+    role: "Vocaloid",
+    imageUrl: "hello-4x.gif",
+  },
+  {
+    name: "Cat Erm",
+    role: "Cat",
+    imageUrl: "catErm-4x.gif",
+    LastSeen: null,
+  },
+  {
+    name: "Marin Kitagawa",
+    email: "",
+    role: "Cosplayer",
+    imageUrl: "my-dressup-darling.jpg",
+    LastSeen: null,
+  },
+];
--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,7 +1,56 @@
-export default function projects() {
-    return (
-      <div>
-        <h1>Hello World</h1>
+import { useState } from "react";
+
+// export default function ImageExpand() {
+//   const [isLage, setIsLarge] = useState(false);
+
+// return (
+// <div className="flex justify-center items-center min-h-screen bg-gray-100">
+//   <img
+//     src="kjermbilde 2025-03-25 131723.png"
+//     alt=""
+//     className={`cursor-pointer transition-all duration-300 ${
+//       isLage ? "w-{500px} h-{500px}" : "w-32 h-32"
+//     } rounded-lg shadow-lg`}
+//     onClick={() => setIsLarge(!isLage)}
+//   />
+// </div>
+// );
+// }
+
+export default function team() {
+  const [isLage, setIsLarge] = useState(false);
+  return (
+    <div>
+      <h2 className="text-4xl mb-3 ml-3 font-semibold">Projects</h2>
+      <div className="bg-white py-14 sm:py-23 rounded-sm">
+        <div className="mx-auto grid max-w-7xl  gap-20 px-6 lg:px-8 xl:grid-cols-3">
+          <>
+            <div className="flex items-center ml-5 gap-x-4">
+              <img
+                src="catErm-4x.gif"
+                alt=""
+                className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+              />
+              <div>
+                <h2 className="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">
+                  Gutendex
+                </h2>
+                <p className="text-gray-500">Bokapplikasjon</p>
+              </div>
+            </div>
+          </>
+          <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <img
+              src="./public/skjermbilde 2025-03-25 131723.png"
+              alt=""
+              className={`cursor-pointer transition-all duration-300 ${
+                isLage ? "w-{500px} h-{500px}" : "w-80 h-89"
+              } rounded-lg shadow-lg`}
+              onClick={() => setIsLarge(!isLage)}
+            />
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
